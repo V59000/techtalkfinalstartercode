7707e0725a885a1481cf27b516961a8c9139c4b4
Oscar Ramirez <oramirez2020@bklawtech.com>
2017-07-07 19:13:33 +0000

    final

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // JavaScript File
 // JavaScript File
+/*global $*/
 $( document ).ready(function() {
-
+$("#click").click(function(){
+    $("#xinfo").text("ScriptED is not only educational, but also fun! You can socialize with students from other schools and enjoy creative learning. ");
+});
 
 });
